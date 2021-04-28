0481d351cc95f62bfbc53f9a3ceabf867c3218c2
Jason Lynch <jason@fugue.co>
2021-04-28 13:20:38 -0400

    [RM-5352] Remove generic yaml and json detectors

---
+++ pkg/loader/cfnjson.go
@@ -0,0 +1,46 @@
+package loader
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+var CfnJsonDetector = NewTypeDetector(&TypeDetector{
+	DetectFile: func(i *InputFile, opts DetectOptions) (Loader, error) {
+		if !opts.IgnoreExt && i.Ext != ".json" {
+			return nil, fmt.Errorf("File does not have .json extension: %v", i.Path)
+		}
+		contents, err := i.ReadContents()
+		if err != nil {
+			return nil, err
+		}
+
+		c := &map[string]interface{}{}
+		if err := json.Unmarshal(contents, c); err != nil {
+			return nil, fmt.Errorf("Failed to parse JSON file %v: %v", i.Path, err)
+		}
+		_, hasTemplateFormatVersion := (*c)["AWSTemplateFormatVersion"]
+		_, hasResources := (*c)["Resources"]
+
+		if !hasTemplateFormatVersion && !hasResources {
+			return nil, fmt.Errorf("Input file is not CloudFormation JSON: %v", i.Path)
+		}
+
+		return &cfnJsonLoader{
+			path:    i.Path,
+			content: c,
+		}, nil
+	},
+})
+
+type cfnJsonLoader struct {
+	path    string
+	content *map[string]interface{}
+}
+
+func (l *cfnJsonLoader) RegulaInput() RegulaInput {
+	return RegulaInput{
+		"filepath": l.path,
+		"content":  l.content,
+	}
+}

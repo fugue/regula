463880ca63e015228e940c01e2bc7770e320b33a
Evan Nemerson <evan.nemerson@fugue.co>
2021-09-07 14:01:40 -0400

    [RM-5916] Add link to rule documentation in output report
    This is currently a bit hackish; I just added code to check the ID at
    the end of the process and inject the link if it's something we have
    remediation documentation for.  Longer term it would probably make
    sense to move the link into the __rego_metadoc__ blocks in the rego
    files, but there is some infrastructure stuff which needs to happen
    first.

--- rego/embedded.go
+++ rego/embedded.go
@@ -2,6 +2,8 @@ package rego
 
 import (
 	"embed"
+
+	"gopkg.in/yaml.v3"
 )
 
 //go:embed lib
@@ -9,3 +11,17 @@ var RegulaLib embed.FS
 
 //go:embed rules
 var RegulaRules embed.FS
+
+//go:embed remediation.yaml
+var regulaRemediations string
+
+type RemediationRuleInfo struct {
+	URL string `yaml:"url"`
+}
+
+var RegulaRemediations map[string]RemediationRuleInfo = nil
+
+func init() {
+	RegulaRemediations = make(map[string]RemediationRuleInfo)
+	yaml.Unmarshal([]byte(regulaRemediations), &RegulaRemediations)
+}
